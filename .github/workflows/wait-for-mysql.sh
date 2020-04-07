23966e5c28ff53629de30cf03980f122f2cafb3a
t-yng <t.yng.greenmonkey@gmail.com>
2020-04-07 23:37:20 +0900

    fix if condition

--- .github/workflows/wait-for-mysql.sh
+++ .github/workflows/wait-for-mysql.sh
@@ -4,7 +4,7 @@ echo "Waiting for MySQL"
 for i in `seq 1 10`;
 do
     result="$(echo '\q' | mysql -h 127.0.0.1 -uroot -P 3306 2>&1 > /dev/null)"
-    if [[ $result = "" ]]; then
+    if [ "$result" = "" ]; then
         echo "Success waiting for MySQL"
         exit 0
     fi
